c9bd6c4149d27a672c8cb48fff7338ce3fe42c3c
Michael Reynolds <michael4reynolds@gmail.com>
2016-06-05 19:21:43 -0400

    complete assignment 4

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+const jsonParser = bodyParser.json()
 
 class Storage {
   constructor() {
@@ -12,6 +14,16 @@ class Storage {
     this.id++
     return item
   }
+
+  remove(id) {
+    return this.items.splice(id, 1)[0]
+  }
+
+  update(id, body) {
+    body.id = id
+    this.items[id] = body
+    return this.items[id]
+  }
 }
 
 const storage = new Storage()
@@ -24,6 +36,36 @@ app.use(express.static('public'))
 
 app.get('/items', (req, res) => {
   res.json(storage.items)
+})
+
+app.post('/items', jsonParser, (req, res) => {
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+
+  let item = storage.add(req.body.name)
+  res.status(201).json(item)
+})
+
+app.delete('/items/:id', (req, res) => {
+  if (!storage.items[req.params.id]) {
+    return res.status(404).json({'item': 'item not found'})
+  }
+  let item = storage.remove(req.params.id)
+  res.status(200).json(item)
+})
+
+app.put('/items/:id', jsonParser, (req, res) => {
+  if (!storage.items[req.params.id]) {
+    return res.status(404).json({'item': 'item not found'})
+  }
+  if (!req.body) {
+    console.log('not found so far')
+    return res.sendStatus(400)
+  }
+
+  let item = storage.update(req.params.id, req.body)
+  res.status(200).json(item)
 })
 
 app.listen(process.env.PORT || 8080)
---
+++ src/server_tradtional.js
@@ -0,0 +1,69 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+
+var Storage = function Storage() {
+  this.items = []
+  this.id = 0
+};
+
+Storage.prototype.add = function add(name) {
+  var item = {name: name, id: this.id}
+  this.items.push(item)
+  this.id++
+  return item
+}
+
+Storage.prototype.remove = function remove(id) {
+  return this.items.splice(id, 1)[0]
+}
+
+Storage.prototype.update = function update(id, body) {
+  body.id = id
+  this.items[id] = body
+  return this.items[id]
+}
+
+var storage = new Storage()
+storage.add('Broad beans')
+storage.add('Tomatoes')
+storage.add('Peppers')
+
+var app = express()
+app.use(express.static('public'))
+
+app.get('/items', function (req, res) {
+  res.json(storage.items)
+})
+
+app.post('/items', jsonParser, function (req, res) {
+  if (!req.body) {
+    return res.sendStatus(400)
+  }
+
+  var item = storage.add(req.body.name)
+  res.status(201).json(item)
+})
+
+app.delete('/items/:id', function (req, res) {
+  if (!storage.items[req.params.id]) {
+    return res.status(404).json({'item': 'item not found'})
+  }
+  var item = storage.remove(req.params.id)
+  res.status(200).json(item)
+})
+
+app.put('/items/:id', jsonParser, function (req, res) {
+  if (!storage.items[req.params.id]) {
+    return res.status(404).json({'item': 'item not found'})
+  }
+  if (!req.body) {
+    console.log('not found so far')
+    return res.sendStatus(400)
+  }
+
+  var item = storage.update(req.params.id, req.body)
+  res.status(200).json(item)
+})
+
+app.listen(process.env.PORT || 8080)
